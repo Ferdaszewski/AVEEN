ae2be1ae3c2f15a436e233d1cf772550fea1d20f
Joshua Ferdaszewski <Ferdaszewski@users.noreply.github.com>
2024-06-26 00:40:54 -0700

    Add aveey web page

--- applications/web/app.py
+++ applications/web/app.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return '''
-        <form action="/echo_user_input" method="POST">
-            <input name="user_input">
-            <input type="submit" value="Submit!">
-        <form>
-    '''
-
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "", type=str)
-    return f'You Entered: "{escape(input_text)}"'
+@app.route("/aveey")
+@app.route("/aveey/<date>")
+def main(date=None):
+    video_url = 'https://bucketeer-3f5efd47-dcff-47cf-ab77-76a0f9c3b9b9.s3.us-east-1.amazonaws.com/videos/2024-06-11-earth.mp4'
+    world_pop = 0
+    space_pop = 0
+    return render_template(
+        'video.html',
+        video_url=video_url,
+        world_pop=world_pop,
+        space_pop=space_pop,
+    )
