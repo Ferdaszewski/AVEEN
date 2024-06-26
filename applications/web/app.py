13b82d567501f464305bd23b2d1890da1b8d2310
Joshua Ferdaszewski <Ferdaszewski@users.noreply.github.com>
2024-06-26 03:36:46 -0700

    Fix SQL bug

--- applications/web/app.py
+++ applications/web/app.py
@@ -12,17 +12,15 @@ app = Flask(__name__)
 def main(date=None):
     with setup_db_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT video_key FROM video_pop ORDER BY video_day desc LIMIT 1")
+            cursor.execute("SELECT v.video_key, s.pop, w.pop FROM video_pop v JOIN space_pop s ON v.space_pop_id = s.id JOIN world_pop w ON v.world_pop_id = w.id ORDER BY v.video_day desc LIMIT 1")
             row = cursor.fetchone()
-            video_key = row if row is None else row[0]
+            video_key, space_pop, world_pop = (None, None, None) if row is None else row
     video_url = f'https://{os.environ.get("BUCKETEER_BUCKET_NAME")}.s3.us-east-1.amazonaws.com/{video_key}'
-    world_pop = 0
-    space_pop = 0
     return render_template(
         'video.html',
         video_url=video_url,
-        world_pop=world_pop,
-        space_pop=space_pop,
+        world_pop=0 if world_pop is None else world_pop,
+        space_pop=0 if space_pop is None else space_pop,
     )
 
 
